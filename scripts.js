e43ce8fa28360c8a39a41b6a2799c237c1467616
zerotriste <alanidesu@gmail.com>
2023-11-14 10:47:09 -0300

    adicionando os code ja feitos

---
+++ scripts.js
@@ -0,0 +1,10 @@
+    let titulo = document.getElementById('tituloprincipal')
+    let h = document.getElementById('horario')
+    let d = new Date()
+    let hora = d.getHours()
+    let minutes = d.getMinutes()
+    h.innerHTML=`agora sao ${hora}:${minutes}H.`
+
+    nome = prompt('olá, por favor nos diga qual e seu nome')
+    titulo.innerHTML=`seja bem vindo(a) ${nome}.`
+    
