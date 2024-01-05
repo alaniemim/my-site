0de8fbc3902c7db44d5e21dc5c7a4ec65a7dbcaf
zerotriste <alanidesu@gmail.com>
2024-01-05 11:11:25 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 let w = document.getElementById('welcome')
 let visitante = prompt('Diga seu nome: ')
-let msg = window.alert(`Caro ${visitante}, a algo escondido nesse site, quando encontrar, me diga a chave secreta e passarei a informacao secreta.`)
+let msg = window.alert(`Ola ${visitante}, a algo escondido nesse site, quando encontrar, me diga a chave secreta para mais informacoes `)
 w.innerHTML = `Bem vindo: ${visitante}!!`
